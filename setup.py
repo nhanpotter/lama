68ab44176260472d5845f6e87881cc4bd2697181
Nguyen Tien Nhan <nhanpotter8a@gmail.com>
2023-02-09 18:28:40 +0800

    update

--- setup.py
+++ setup.py
@@ -3,4 +3,6 @@ import setuptools
 setuptools.setup(
     name="saicinpainting",
     packages=setuptools.find_packages(),
+    package_data={'': ['models/ade20k/color150.mat']},
+    include_package_data=True,
 )
