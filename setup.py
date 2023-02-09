50bb613c2d8ae8a673c640f9db1b2201a0cde4d7
Nguyen Tien Nhan <nhanpotter8a@gmail.com>
2023-02-09 18:34:01 +0800

    add manifest

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ import setuptools
 setuptools.setup(
     name="saicinpainting",
     packages=setuptools.find_packages(),
-    package_data={'models': ['models/ade20k/color150.mat']},
-    include_package_data=True,
+    # package_data={'models': ['models/ade20k/color150.mat']},
+    # include_package_data=True,
 )
