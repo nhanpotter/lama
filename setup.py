ada454e5b7babc8a3a9351bcd64629005cd4befd
Nguyen Tien Nhan <nhanpotter8a@gmail.com>
2023-02-09 11:45:32 +0800

    finalize

--- setup.py
+++ setup.py
@@ -2,5 +2,6 @@ import setuptools
 
 setuptools.setup(
     name="saicinpainting",
-    packages=setuptools.find_packages('saicinpainting')
+    packages=setuptools.find_packages('saicinpainting'),
+    package_dir={'': 'saicinpainting'}
 )
