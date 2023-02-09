78bbfc821b503caa651b4adec3c6f18207f6b994
Nguyen Tien Nhan <nhanpotter8a@gmail.com>
2023-02-09 12:29:29 +0800

    rm requirements

--- setup.py
+++ setup.py
@@ -1,18 +1,6 @@
 import setuptools
 
-
-def load_requirements():
-    requirements_file_name = "requirements.txt"
-    requires = []
-    with open(requirements_file_name) as f:
-        for line in f:
-            if line:
-                requires.append(line.strip())
-    return requires
-
-
 setuptools.setup(
     name="saicinpainting",
     packages=setuptools.find_packages(),
-    install_requires=load_requirements(),
 )
